7d19327b366b1c0fe934a6eba596dac1d5a062cc
Varun Sharma <varunsharma@pacefin.in>
2023-09-29 13:09:56 +0530

    setup again

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pocketful-tech/throttling
+
+go 1.20
---
+++ throttling_sync.go
@@ -0,0 +1,31 @@
+package throttling
+
+import (
+	"sync"
+	"time"
+)
+
+type APIThrottler struct {
+	RequestsPerSecond int
+	LastRequestTime   time.Time
+	Mu                sync.Mutex
+	ClientName        string
+	ClientIp          string
+	VendorName        string
+}
+
+func (t *APIThrottler) Throttle() {
+	t.Mu.Lock()
+	defer t.Mu.Unlock()
+
+	currentTime := time.Now()
+	timeSinceLastRequest := currentTime.Sub(t.LastRequestTime)
+	desiredInterval := time.Second / time.Duration(t.RequestsPerSecond)
+
+	if timeSinceLastRequest < desiredInterval {
+		sleepDuration := desiredInterval - timeSinceLastRequest
+		time.Sleep(sleepDuration)
+	}
+
+	t.LastRequestTime = time.Now()
+}
