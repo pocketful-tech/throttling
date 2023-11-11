adf39606a599f2ad272009d964571c1313803524
varunsharma4 <varunsharma@pacefin.in>
2023-11-11 15:53:42 +0530

    throttling logic change

--- throttling_sync.go
+++ throttling_sync.go
@@ -11,9 +11,28 @@ type APIThrottler struct {
 	Mu                sync.Mutex
 	ClientName        string
 	VendorName        string
+	Count             int
 }
 
 func (t *APIThrottler) Throttle() {
+	t.Mu.Lock()
+	defer t.Mu.Unlock()
+
+	if t.Count == 0 {
+		t.LastRequestTime = time.Now()
+	}
+	t.Count++
+
+	if t.Count >= t.RequestsPerSecond {
+		currTime := time.Now()
+		sleepDurationNew := time.Second - currTime.Sub(t.LastRequestTime)
+		time.Sleep(sleepDurationNew)
+		t.Count = 0
+	}
+
+}
+
+func (t *APIThrottler) ThrottleDeprecate() {
 	t.Mu.Lock()
 	defer t.Mu.Unlock()
 
